b55c53b81d2bd53ee7203f7161e001e75eb590db
jf10rez <jflorezlondono@hotmail.com>
2022-06-14 17:06:21 -0500

    Crud reserves finsihed

--- controllers/reserveController.js
+++ controllers/reserveController.js
@@ -1,4 +1,6 @@
 const { response } = require('express')
+const dayjs = require("dayjs")
+
 const Reserve = require('../models/ReserveModel')
 
 
@@ -25,35 +27,141 @@ const getReserves = async( req, res = response ) => {
 
 const createReserve = async( req, res = response ) => {
 
-    // console.log( req.body )
+    const { room, dateInitial } = req.body
 
-    // res.status(200).json({
-    //     ok: true
-    // })
-    // const reserves = new Reserve( req.body )
+    try {
 
-    // try {
+        //exist reserve in this room?
+        const existReserve = await Reserve.find( { room, dateInitial } )
+
+        if( existReserve.length > 0 ){
+            res.status(412).json({
+                ok: false,
+                message: 'La habitación ya está reservada para esta fecha'
+            })
+            return
+        }
+
+        const reserves = new Reserve( req.body )
+
+        const reserveSave = await reserves.save()
+
+        res.status(200).json({
+            ok: true,
+            reserve: reserveSave
+        })
+
+    } catch (error) {
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            message: 'Se produjo un error con el servidor'
+        })
+    }
+
+}
+
+const updateReserve = async( req, res = response ) => {
+
+    const { room, dateInitial } = req.body
+    const id = req.params.id
+
+    try {
+
+        //exist reserve in this room?
+        const existReserve = await Reserve.find( { room, dateInitial } )
+
+        if( existReserve.length > 0 ){
+            res.status(412).json({
+                ok: false,
+                message: 'La habitación ya está reservada para esta fecha'
+            })
+            return
+        }
+
+        const reserve = await Reserve.findById( id )
+
+        if( !reserve ){
+            res.status(404).json({
+                ok: false,
+                message: 'No se encontró la reserva'
+            })
+            return;
+        }
+
+        if( reserve.user.toString() !== req.uid ){
+            res.status(401).json({
+                ok: false,
+                message: 'El usuario no puede modificar esta habitación'
+            })
+        }
+
+        const updateReserve = {
+            ...req.body,
+            user: req.uid
+        }
+
+        const reserveUpdated = await Reserve.findByIdAndUpdate( id, updateReserve, { new: true } )
+
+        res.status(200).json({
+            ok: true,
+            reserve: reserveUpdated
+        })
+
+    } catch (error) {
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            message: 'Se produjo un error con el servidor'
+        })
+    }
+
+}
+
+const deleteReserve = async( req, res = response ) => {
+
+    const id = req.params.id
+
+    try {
+
+        const reserve = await Reserve.findById( id )
+
+        if( !reserve ){
+            res.status(404).json({
+                ok: false,
+                message: 'No se encontró la reserva'
+            })
+            return;
+        }
+
+        if( reserve.user.toString() !== req.uid ){
+            res.status(401).json({
+                ok: false,
+                message: 'El usuario no puede modificar esta habitación'
+            })
+        }
+
+        await Reserve.findByIdAndDelete( id )
+
+        res.status(200).json({
+            ok: true,
+            message: 'Se eliminó la reserva con éxito',
+            id
+        })
         
-    //     reserves.user = req.uid
-
-    //     const reserveSave = await reserves.save()
-
-    //     res.status(200).json({
-    //         ok: true,
-    //         reserve: reserveSave
-    //     })
-
-    // } catch (error) {
-    //     console.log( error )
-    //     res.status(500).json({
-    //         ok: false,
-    //         message: 'Se produjo un error con el servidor'
-    //     })
-    // }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            message: 'Se produjo un error con el servidor'
+        })
+    }
 
 }
 
 module.exports = {
     getReserves,
-    createReserve
+    createReserve,
+    updateReserve,
+    deleteReserve
 }
--- routes/reserveRoute.js
+++ routes/reserveRoute.js
@@ -5,19 +5,55 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 const router = Router()
 
-const { getReserves, createReserve } = require('../controllers/reserveController')
+const { 
+    getReserves, 
+    createReserve, 
+    updateReserve, 
+    deleteReserve 
+} = require('../controllers/reserveController')
+
+//Middlewares
+const { isDate } = require('../helpers/isDate')
+const { isObjectId } = require('../helpers/isObjectId')
+const { validateFields } = require('../middlewares/validate-fields')
 const { validateJWT } = require('../middlewares/validate-jwt')
+const { validateObjectId } = require('../middlewares/validate-objectId')
 
 // Host + api/reserve/
 router.get( '/', validateJWT , getReserves )
 
-// router.post( 
-//     '/create',
-//     [
-//         check('')
-//     ]
-//     ,
-//     createReserve )
+router.post( 
+    '/create',
+    [
+        check('nameClient', 'El nameClient es obligatorio').not().isEmpty(),
+        check('emailClient', 'El emailClient es obligatorio').isEmail(),
+        check('celPhone', 'El celPhone es obligatorio').isNumeric(),
+        check('dateInitial', 'El dateInitial es obligatorio').custom( isDate ),
+        check('dateFinal', 'El dateFinal es obligatorio').custom( isDate ),
+        check('user', 'El user no es valido').custom( isObjectId ),
+        check('room', 'El room no es valido').custom( isObjectId ),
+        validateFields
+    ]
+    ,
+    createReserve )
 
+router.put( 
+    '/:id',
+    [
+        check('nameClient', 'El nameClient es obligatorio').not().isEmpty(),
+        check('emailClient', 'El emailClient es obligatorio').isEmail(),
+        check('celPhone', 'El celPhone es obligatorio').isNumeric(),
+        check('dateInitial', 'El dateInitial es obligatorio').custom( isDate ),
+        check('dateFinal', 'El dateFinal es obligatorio').custom( isDate ),
+        check('user', 'El user no es valido').custom( isObjectId ),
+        check('room', 'El room no es valido').custom( isObjectId ),
+        validateFields,
+        validateJWT,
+        validateObjectId
+    ],
+    updateReserve
+    )
+
+    router.delete( '/:id', [validateJWT, validateObjectId], deleteReserve )
 
 module.exports = router
