0da8cefcd3086f832003d9b89a23911ab1f03f5a
jf10rez <jflorezlondono@hotmail.com>
2022-06-17 14:50:43 -0500

    delete field image, and path added

--- models/ImageModel.js
+++ models/ImageModel.js
@@ -6,15 +6,21 @@ const ImageSchema = Schema({
         type: String,
         required: true
     },
-    image: {
-        data: Buffer,
-        contentType: String
+    path: {
+        type: String,
+        required: true
     },
     room: {
         type: Schema.Types.ObjectId,
         ref: 'Room',
         required: true
     }
+})
+
+ImageSchema.method('toJSON', function(){//Para devolver la info en el controlador con el objeto modificado
+    const { __v, _id, ...object } = this.toObject()
+    object.id = _id
+    return object
 })
 
 module.exports = model( 'Image', ImageSchema )
