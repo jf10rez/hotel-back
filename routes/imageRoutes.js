806ad1a686a6e07289d62416677a3d80126d2c7f
jf10rez <jflorezlondono@hotmail.com>
2022-06-17 14:48:37 -0500

    Change shape to send images with cloudinary v2

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -5,32 +5,44 @@
 const { Router } = require("express");
 const router = Router();
 
-const { storeImage, imagesByRoom, updateImage, deleteImage } = require("../controllers/imageController");
+const {
+  imagesByRoom,
+  deleteImage,
+  uploadImageToCloud,
+} = require("../controllers/imageController");
 const { isObjectId } = require("../helpers/isObjectId");
 const { upload } = require("../helpers/multer-validations");
-const { check } = require('express-validator')
-
+const { check } = require("express-validator");
 
 //Middlewares
 const { validateJWT } = require("../middlewares/validate-jwt");
 const { validateObjectId } = require("../middlewares/validate-objectId");
 const { validateFields } = require("../middlewares/validate-fields");
 
-router.use( validateJWT )
+router.use(validateJWT);
+
+// router.post(
+//   "/",
+//   [
+//     upload.array("images", 12),
+//     check("idRoom", "El idRoom no es valido").custom(isObjectId),
+//     validateFields,
+//   ],
+//   storeImage
+// );
 
 router.post(
-    "/",
-    [
-        upload.array("images", 12),
-        check('idRoom', 'El idRoom no es valido').custom( isObjectId ),
-        validateFields
-    ],
-    storeImage);
+  "/",
+  [
+    upload.single("image"),
+    check("idRoom", "El idRoom no es valido").custom(isObjectId),
+    validateFields,
+  ],
+  uploadImageToCloud
+);
 
-router.get( '/:idRoom', validateObjectId , imagesByRoom )
+router.get("/:idRoom", validateObjectId, imagesByRoom);
 
-router.delete( '/:id', deleteImage )
-
-// router.put( '/', upload.single('images') , updateImage )
+router.delete("/:id", deleteImage);
 
 module.exports = router;
